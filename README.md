7765b20c5ddb2d6530f0f6c4ee3c0697c6e775c9
saranya chinnadurai <saranyachinnadurai@saranyas-MacBook-Air.local>
2025-10-22 15:10:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Install virtual environment
+
+    python3.12 -m venv .venv_playwright
+    
+Activate/ Deactivate VM :
+
+    cd playwright-pom
+    source .venv_playwright/bin/activate
+    deactivate
+
+If you are using pycharm, add `/venv_playwright/bin/python` to the Project Interpretre
+
+Install the libraries after activating virtual env :
+    pip3 install -r requirements.txt
+
+
+
---
+++ test_sample.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import Page
+def test_playwrightBasics(playwright): # playwrite attribute is the global fixture available in pytest_playwright
+    browser = playwright.chromium.launch(headless=False) # google chrome , microsoft edge
+    context = browser.new_context() # opening an ingonoto window
+    page = context.new_page() # open a tab
+    page.goto("https://nationalpost.com/") # go to url
+
+# page argument - chromium headless mode default , 1 single context
+def test_playwrightShortCut(page:Page):
+    page.goto("https://nationalpost.com/")
